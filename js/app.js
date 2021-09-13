672a02460337b944789f70c242f090402cc97a17
Faisalnk4045 <foysalnk9095@gmail.com>
2021-09-13 13:12:07 +0600

    product card rating and styles updated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products?limit=5`;
+  const url = `https://fakestoreapi.com/products?limit=9`;
   // const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
@@ -14,19 +14,59 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+    div.innerHTML = `
+      <div class="card border rounded-3 border-secondary single-product">
+        <div>
+          <img class="product-image" src=${image}></img>
+        </div>
+
+        <div class="card-body">
+          <h5>${product.title}</h5>
+        </div>
+      
+        <div class="card-footer border-top-0 bg-white">
+          <p class="text-muted">Category: ${product.category}</p>
+          <small class="text-muted pe-3">Reviews: ${product.rating.count}</small>
+          <small class="text-muted">Rating: ${product.rating.rate}</small>
+          <h5>Price: $${product.price}</h5>
+          <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success mt-3">add to cart</button>
+          <button onclick="getProductDetails(${product.id})" id="details-btn" class="btn btn-danger mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+        </div>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+const getProductDetails = (id) => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showProductDetails(data))
+}
+
+const showProductDetails = (product) => {
+  const title = product.title;
+  const category = product.category;
+  const description = product.description;
+  const price = product.price;
+  const totalReview = product.rating.count;
+  const rating = product.rating.rate;
+
+  const element = document.getElementById('product-details');
+  element.textContent = '';
+  const div = document.createElement('div');
+  div.classList.add('card-body');
+  div.innerHTML = `
+  <h5 class="card-title">${title}</h5>
+  <p class="card-text">Category: ${category}</p>
+  <p class="card-text">Price: $${price}</p>
+  <p class="card-text">Reviews: ${totalReview}</p>
+  <p class="card-text">Rating: ${rating}</p>
+  <p class="card-text">${description}</p>
+  `;
+  element.appendChild(div);
+}
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -40,7 +80,6 @@ const addToCart = (id, price) => {
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
-  console.log('old-price : ', converted);
   return converted;
 };
 
@@ -48,7 +87,6 @@ const getInputValue = (id) => {
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
-  console.log('new-price: ', convertPrice);
   const total = convertedOldPrice + convertPrice;
   document.getElementById(id).innerText = total.toFixed(2);
 };
